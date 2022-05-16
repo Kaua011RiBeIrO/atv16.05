4d18d69901a3d7fa9d0de2c8998a9ad6887dfb2a
Kaua011RiBeIrO <102737682+Kaua011RiBeIrO@users.noreply.github.com>
2022-05-16 10:34:55 -0300

    Add files via upload

---
+++ laços.js
@@ -0,0 +1,60 @@
+/*console.log('Ola, tudo bem?')
+console.log('Ola, tudo bem?')
+console.log('Ola, tudo bem?')
+console.log('Ola, tudo bem?')
+console.log('Ola, tudo bem?')
+console.log('Ola, tudo bem?')*/
+
+/*let x = 1
+while (x <= 10){
+    console.log('Ola, tudo bem?')
+    x = x + 1
+}
+
+//faça um laço que repita o seu nome 3 vezes*/
+
+/*let n = 1
+while (n < 3){
+    console.log('Kaua')
+    n = n + 1
+}*/
+
+//faça um laço que mostre os valores do contador.
+
+/*let x = 3
+while (x <= 10){
+    console.log(x)
+    x = x + 3
+}*/
+
+ /*atv - 1
+ let x = 1
+ while (x <= 100){
+     console.log(x)
+     x= x +1
+ }*/
+
+ /*atv - 2
+ let x = 50
+ while (x <= 100){
+     console.log(x)
+     x= x +1
+ }*/
+
+//atv - 3
+/* let x = 10
+ while (x >= 1){
+     console.log(x)
+     x = x - 1
+ }*/
+
+ /*atv - 4
+ let x = 0
+while (x <= 20){
+    console.log(x)
+    x = x + 2
+}*/
+
+
+
+
